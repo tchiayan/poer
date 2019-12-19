b1c98c2290cd659ac5bb3fcdd396d26bbb72635a
Tan Chia Yan <tchiayan@gmail.com>
2019-12-20 01:56:39 +0800

    Updates

---
+++ precache-manifest.310a665dc5827544fc383102b8be4eb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b4b93d3270fec335d4017dcb3f40c0",
+    "url": "/poer/index.html"
+  },
+  {
+    "revision": "03d58d08968759d83ea1",
+    "url": "/poer/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a623833d2a96ed433423",
+    "url": "/poer/static/css/main.eb7a368f.chunk.css"
+  },
+  {
+    "revision": "03d58d08968759d83ea1",
+    "url": "/poer/static/js/2.90aee4ad.chunk.js"
+  },
+  {
+    "revision": "296cd8aec25a3e3a4d54683ad6e3ea08",
+    "url": "/poer/static/js/2.90aee4ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a623833d2a96ed433423",
+    "url": "/poer/static/js/main.d5933379.chunk.js"
+  },
+  {
+    "revision": "871aab6d6a8ceac06fc6",
+    "url": "/poer/static/js/runtime-main.071a1f7f.js"
+  }
+]);
